9d1e5b1f5d141d0ec9b417c72f361bb6d3f6676e
thandoncube1 <ncubethando20@gmail.com>
2024-09-08 23:41:51 -0400

    Looking at my python program.

---
+++ control_statements.py
@@ -0,0 +1,14 @@
+# Starting the python project
+# This is a guessing game that I use to learn a new language
+def isGuessCorrect(number, secretNumber):
+    if (number == secretNumber):
+        print("Congratulations! You guessed correctly", secretNumber)
+
+    if ():
+    elif ():
+    else
+
+
+if ("__main__" == __main__):
+    # The main entry of the application
+
