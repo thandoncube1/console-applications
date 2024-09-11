3e385fa80cade3002d3b30489fa40229858d601e
thandoncube1 <ncubethando20@gmail.com>
2024-09-10 20:10:17 -0400

    Adding strings with immutable elements.

--- helloNew.d
+++ helloNew.d
@@ -1,5 +1,22 @@
 import std.stdio;
 
+dchar[] mix(immutable dchar[] first,
+            immutable dchar[] second) {
+    dchar[] result;
+    int i;
+
+    for (i = 0; (i < first.length) && (i < second.length); ++i) {
+        result ~= first[i];
+        result ~= second[i];
+    }
+
+    result ~= first[i..$];
+    result ~= second[i..$];
+
+    return result;
+}
+
+
 int main() {
     char[] name;
 
