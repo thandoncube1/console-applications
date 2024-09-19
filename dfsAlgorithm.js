19eb93449d1988f3319ffca97c1fa395d3d4d489
thandoncube1 <ncubethando20@gmail.com>
2024-09-18 21:16:30 -0400

    Working on the dfs, search traversal.

--- dfsAlgorithm.js
+++ dfsAlgorithm.js
@@ -1,6 +1,6 @@
 // Adding a sample directed graph structure to look over
 // Example:
-const graph = {
+const europeGraph = {
   Paris: ['London', 'Madrid'],
   London: ['Berlin', 'Amsterdam'],
   Berlin: ['Warsaw', 'Prague'],
@@ -18,3 +18,25 @@ const graph = {
 };
 
 // Write the algorithm that returns a visited nodes as an array
+
+function dfs(graph, start) {
+  const visited = new Set();
+  const result = new Array(); // Typed array of size 8.
+
+  function internalDFS(node) {
+    if(visited.has(node)) return;
+    visited.add(node);
+    result.push(node);
+
+    graph[node].forEach((nextNode) => {
+      if (!visited.has(nextNode)) {
+        internalDFS(nextNode);
+      }
+    });
+  }
+
+  internalDFS(start);
+  return result;
+}
+
+console.log(dfs(europeGraph, "Paris"));
