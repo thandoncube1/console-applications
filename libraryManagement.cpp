79bf2b96f89bbe69575eb80fff4d2f2d6ef59e62
thandoncube1 <ncubethando20@gmail.com>
2024-09-23 01:42:28 -0400

    Building a library application.

--- libraryManagement.cpp
+++ libraryManagement.cpp
@@ -5,12 +5,58 @@ struct Person {
   std::string firstName;
   std::string lastName;
   int age = 0;
+  std::string email;
+  std::string picture;
+  char gender;
+  std::string phoneNumber = 0;
   bool isLibraryStuff = 0;
   bool isActiveMember = 0;
+};
+
+class Library {
+  private:
+    std::string libraryName;
+    std::string location;
+    std::string openingHours;
+    double contactNumber;
   
+  public:
+    Library() {}
+    Library(std::string libraryName, std::string location, std::string openingHours, double contactNumber){
+      this->libraryName = libraryName;
+      this->location = location;
+      this->openingHours = openingHours;
+      this->contactNumber = contactNumber;
+    }
+
+    ~Library();
+
+    // Create getters and setters for the Library application
+    void setLibraryName(std::string libraryName) {
+      this->libraryName = libraryName;
+    }
+
+    std::string getLibraryName() {
+      return this->libraryName;
+    }
+
+    void setOpeningHours(std::string openingHours) {
+      this->openingHours = openingHours;
+    }
+
+    std::string getOpeningHours() {
+      return this->openingHours;
+    }
+
+    double getContactNumber() {
+      return this->contactNumber;
+    }
+
+
+
 };
 
 int main() {
-
+  std::cout << "Welcome to the Library Application \n";
   return 0;
 }
