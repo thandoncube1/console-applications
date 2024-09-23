a2aec647bf76e1a4415800409635577a1f8a5a2c
thandoncube1 <ncubethando20@gmail.com>
2024-09-23 02:11:10 -0400

    Working on the MySQL Piece of the application.

--- libraryManagement.cpp
+++ libraryManagement.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include <string>
 
+/*
+  Include directly the different
+  headers from cppconn/ and mysql_driver.h + mysql_util.h
+  (and mysql_connection.h). This will reduce your build time!
+*/
+#include "mysql_connection.h"
+
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+
 struct Person {
   std::string firstName;
   std::string lastName;
