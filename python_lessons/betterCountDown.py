81beaa55e3c75a0b84862754287fd748c7f7e113
thandoncube1 <ncubethando20@gmail.com>
2024-09-26 03:49:53 -0400

    Working with time in minutes and seconds.

--- python_lessons/betterCountDown.py
+++ python_lessons/betterCountDown.py
@@ -15,15 +15,38 @@ except ValueError:
     print("Wrong input selected, Please enter either (m) or (s)")
 
 if (select.strip().upper() == "m".upper()):
-    timeInput = int(input("Please enter time in minutes: "))
+    try:
+        timeInput = int(input("Please enter time in minutes: "))
+    except ValueError:
+        print("Wrong input selected, Please enter time in minutes.")
+
+    minute = timeInput
+    second = 0
 elif (select.strip().upper() == "s".upper()):
-    timeInput = int(input("Please enter time in seconds: "))
+    try:
+        timeInput = int(input("Please enter time in seconds: "))
+    except ValueError:
+        print("Wrong input selected, Please enter time in seconds")
+
     minute = timeInput // 60
     second = timeInput % 60
     if timeInput < 60:
         second = timeInput
         minute = 0
 
+# Write the loop structures that deal with time
+while minute >= 0:
+    while second >= 0:
+        if (second < 10):
+            print(f"Count down in {minute}:{second:02d}")
+        else: print(f"Count down in {minute}:{second}")
+        second -= 1
+        sleep(1)
+    minute -= 1
+    second = 59
+
+print("Our Rocket has Launched 🚀", end=" ")
+print("Thank you for supporting out mission.")
 
 
 
